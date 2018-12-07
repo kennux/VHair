6a4368e7215ee7d4b9b77fe63ead74323d8477f9
Kenneth <kennux@ymail.com>
2018-12-07 19:11:37 +0100

    New renderer implemented

---
+++ Assets/VHair/Code/Rendering/StandardRenderer/VHairStandardRenderer.cs
@@ -0,0 +1,244 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Jobs;
+using Unity.Collections;
+using Unity.Collections.LowLevel.Unsafe;
+using Unity.Mathematics;
+using Unity.Burst;
+
+namespace VHair
+{
+	/// <summary>
+	/// A "standard" renderer implementation that does nothing else except synchronizing the simulation data to a mesh generated at runtime.
+	/// It will only provide a mesh to a mesh renderer and not do any rendering itself at all.
+	/// </summary>
+	[RequireComponent(typeof(MeshRenderer), typeof(MeshFilter))]
+	public class VHairStandardRenderer : HairRenderer
+	{
+		public enum UVDistributionStrategy
+		{
+			ALONG_STRAND_Y
+		}
+
+		struct TriangulatedSegment
+		{
+			public int hairVertexIndex1;
+			public int hairVertexIndex2;
+			
+			public int strandVertexIndex1;
+			public int strandVertexIndex2;
+			public int strandIndex;
+			
+			// v1-------v2
+			// |		|
+			// |		|
+			// |		|
+			// v3-------v4
+			public int v1;
+			public int v2;
+			public int v3;
+			public int v4;
+		}
+
+		[BurstCompile]
+		struct PositionUpdateJob : IJobParallelFor
+		{
+			public float3 facing;
+			public float4x4 worldToLocal;
+			public float hairWidth;
+
+			[ReadOnly]
+			public NativeArray<float3> hairVertices;
+			[ReadOnly]
+			public NativeArray<TriangulatedSegment> segments;
+			
+			[NativeDisableParallelForRestriction]
+			[WriteOnly]
+			public NativeArray<float3> vertices;
+
+			[NativeDisableParallelForRestriction]
+			[WriteOnly]
+			public NativeArray<float3> normals;
+
+			public void Execute(int index)
+			{
+				TriangulatedSegment segment = this.segments[index];
+				float3 v1, v2, v3, v4, n1, n2, n3, n4, p1, p2;
+				
+				p1 = hairVertices[segment.hairVertexIndex1];
+				p2 = hairVertices[segment.hairVertexIndex2];
+
+				float3 p2To1 = (p2 - p1);
+				float3 facingDir = ((p2 - facing) + (p1 - facing)) / 2f;
+
+				float3 left = math.normalize(math.cross(p2To1, facingDir));
+				v1 = p1 + hairWidth * left;
+				v2 = p1 + hairWidth * -left;
+				v3 = p2 + hairWidth * left;
+				v4 = p2 + hairWidth * -left;
+				
+				v1 = math.mul(worldToLocal, new float4(v1.x, v1.y, v1.z, 1)).xyz;
+				v2 = math.mul(worldToLocal, new float4(v2.x, v2.y, v2.z, 1)).xyz;
+				v3 = math.mul(worldToLocal, new float4(v3.x, v3.y, v3.z, 1)).xyz;
+				v4 = math.mul(worldToLocal, new float4(v4.x, v4.y, v4.z, 1)).xyz;
+
+				vertices[segment.v1] = v1;
+				vertices[segment.v2] = v2;
+				vertices[segment.v3] = v3;
+				vertices[segment.v4] = v4;
+
+				n1 = n2 = n3 = n4 = math.mul(quaternion.LookRotation(math.normalize(p2 - p1), new float3(0,1,0)), new float3(0,1,0));
+				normals[segment.v1] = n1;
+				normals[segment.v2] = n2;
+				normals[segment.v3] = n3;
+				normals[segment.v4] = n4;
+			}
+		}
+
+		public Transform facing;
+		public float hairWidth = .1f;
+		public UVDistributionStrategy uvDistStrat;
+		
+		private new MeshRenderer renderer;
+		private MeshFilter filter;
+		private Mesh mesh;
+		
+		private NativeArray<float3> strandVertices;
+		private NativeArray<float3> vertices;
+		private NativeArray<float3> uvs;
+		private NativeArray<float3> normals;
+		private NativeArray<int> indices;
+		private NativeArray<TriangulatedSegment> segments;
+
+		public void Awake()
+		{
+			this.renderer = GetComponent<MeshRenderer>();
+			this.filter = GetComponent<MeshFilter>();
+			this.mesh = new Mesh();
+			this.mesh.indexFormat = UnityEngine.Rendering.IndexFormat.UInt32;
+			this.mesh.MarkDynamic();
+			this.filter.sharedMesh = this.mesh;
+		}
+
+		public void Start()
+		{
+			List<TriangulatedSegment> segments = new List<TriangulatedSegment>();
+
+			// Triangulate
+			var strands = this.instance.strands.cpuReference;
+			{
+				int vCtr = 0;
+				for (int sIndex = 0; sIndex < strands.Length; sIndex++)
+				{
+					HairStrand strand = strands[sIndex];
+					for (int vIndex = strand.firstVertex; vIndex < strand.lastVertex - 1; vIndex++)
+					{
+						// Build segment
+						TriangulatedSegment segment = new TriangulatedSegment()
+						{
+							hairVertexIndex1 = vIndex,
+							hairVertexIndex2 = vIndex + 1,
+							v1 = vCtr++,
+							v2 = vCtr++,
+							v3 = vCtr++,
+							v4 = vCtr++,
+							strandIndex = sIndex,
+							strandVertexIndex1 = vIndex-strand.firstVertex,
+							strandVertexIndex2 = (vIndex+1)-strand.firstVertex
+						};
+
+						segments.Add(segment);
+					}
+				}
+			}
+			this.segments = new NativeArray<TriangulatedSegment>(segments.Count, Allocator.Persistent);
+			this.segments.CopyFrom(segments.ToArray());
+
+			// Distribute uv and indices
+			this.uvs = new NativeArray<float3>(segments.Count * 4, Allocator.Persistent);
+			this.normals = new NativeArray<float3>(segments.Count * 4, Allocator.Persistent);
+			this.vertices = new NativeArray<float3>(segments.Count * 4, Allocator.Persistent);
+			this.indices = new NativeArray<int>(segments.Count * 6, Allocator.Persistent);
+			
+			for (int i = 0; i < segments.Count; i++)
+			{
+				var segment = segments[i];
+
+				// Calc uv
+				float3 uv1, uv2, uv3, uv4;
+				uv1 = uv2 = uv3 = uv4 = float3.zero;
+				switch (this.uvDistStrat)
+				{
+					case UVDistributionStrategy.ALONG_STRAND_Y:
+						{
+							HairStrand hs = strands[segment.strandIndex];
+							float y = segment.strandVertexIndex1 / (hs.lastVertex - hs.firstVertex);
+							float y2 = segment.strandVertexIndex2 / (hs.lastVertex - hs.firstVertex);
+							
+							uv1 = new float3(0, y, 0);
+							uv2 = new float3(1, y, 0);
+							uv3 = new float3(0, y2, 0);
+							uv4 = new float3(1, y2, 0);
+						}
+						break;
+				}
+				
+				this.uvs[segment.v1] = uv1;
+				this.uvs[segment.v2] = uv2;
+				this.uvs[segment.v3] = uv3;
+				this.uvs[segment.v4] = uv4;
+
+				// Write indices
+				this.indices[(i * 6)] = segment.v1;
+				this.indices[(i * 6)+1] = segment.v2;
+				this.indices[(i * 6)+2] = segment.v4;
+				this.indices[(i * 6)+3] = segment.v4;
+				this.indices[(i * 6)+4] = segment.v3;
+				this.indices[(i * 6)+5] = segment.v1;
+			}
+			
+			this.uvs.CopyFrom(uvs.ToArray());
+			this.indices.CopyFrom(indices.ToArray());
+
+			this.strandVertices = new NativeArray<float3>(this.instance.asset.vertexCount, Allocator.Temp);
+			NoAllocHelpers.SetMesh(this.mesh, this.vertices, this.uvs, this.normals, this.indices);
+		}
+
+		private JobHandle handle;
+		public unsafe void LateUpdate()
+		{
+			var vertices = this.instance.vertices.cpuReference;
+			fixed (Vector3* pVertices = vertices)
+			{
+				UnsafeUtility.MemCpy(this.strandVertices.GetUnsafePtr(), (void*)pVertices, UnsafeUtility.SizeOf<Vector3>() * vertices.Length);
+			}
+
+			handle = new PositionUpdateJob()
+			{
+				normals = this.normals,
+				segments = this.segments,
+				hairVertices = this.strandVertices,
+				vertices = this.vertices,
+				facing = this.facing.position,
+				hairWidth = this.hairWidth,
+				worldToLocal = this.transform.worldToLocalMatrix
+			}.Schedule(this.segments.Length, 16);
+
+			JobHandle.ScheduleBatchedJobs();
+			handle.Complete();
+			NoAllocHelpers.SetMesh(this.mesh, this.vertices, null, this.normals);
+			this.mesh.UploadMeshData(false);
+		}
+
+		public void OnDestroy()
+		{
+			this.vertices.Dispose();
+			this.uvs.Dispose();
+			this.normals.Dispose();
+			this.indices.Dispose();
+			this.strandVertices.Dispose();
+			this.segments.Dispose();
+		}
+	}
+}
---
+++ Assets/VHair/Code/Utility/NoAllocUtility.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Unity.Collections.LowLevel.Unsafe;
+using Unity.Collections;
+using UnityEngine;
+using Unity.Mathematics;
+
+/// <summary>
+/// Provides access to the internal UnityEngine.NoAllocHelpers methods.
+/// 
+/// Taken from https://forum.unity.com/threads/nativearray-and-mesh.522951/#post-3842671
+/// </summary>
+public static class NoAllocHelpers
+{
+    private static readonly Dictionary<Type, Delegate> ExtractArrayFromListTDelegates = new Dictionary<Type, Delegate>();
+    private static readonly Dictionary<Type, Delegate> ResizeListDelegates = new Dictionary<Type, Delegate>();
+ 
+    /// <summary>
+    /// Extract the internal array from a list.
+    /// </summary>
+    /// <typeparam name="T"><see cref="List{T}"/>.</typeparam>
+    /// <param name="list">The <see cref="List{T}"/> to extract from.</param>
+    /// <returns>The internal array of the list.</returns>
+    public static T[] ExtractArrayFromListT<T>(List<T> list)
+    {
+		Delegate obj;
+        if (!ExtractArrayFromListTDelegates.TryGetValue(typeof(T), out obj))
+        {
+            var ass = Assembly.GetAssembly(typeof(Mesh)); // any class in UnityEngine
+            var type = ass.GetType("UnityEngine.NoAllocHelpers");
+            var methodInfo = type.GetMethod("ExtractArrayFromListT", BindingFlags.Static | BindingFlags.Public)
+                .MakeGenericMethod(typeof(T));
+ 
+            obj = ExtractArrayFromListTDelegates[typeof(T)] = Delegate.CreateDelegate(typeof(Func<List<T>, T[]>), methodInfo);
+        }
+ 
+        var func = (Func<List<T>, T[]>)obj;
+        return func.Invoke(list);
+    }
+ 
+    /// <summary>
+    /// Resize a list.
+    /// </summary>
+    /// <typeparam name="T"><see cref="List{T}"/>.</typeparam>
+    /// <param name="list">The <see cref="List{T}"/> to resize.</param>
+    /// <param name="size">The new length of the <see cref="List{T}"/>.</param>
+    public static void ResizeList<T>(List<T> list, int size)
+    {
+		Delegate obj;
+        if (!ResizeListDelegates.TryGetValue(typeof(T), out obj))
+        {
+            var ass = Assembly.GetAssembly(typeof(Mesh)); // any class in UnityEngine
+            var type = ass.GetType("UnityEngine.NoAllocHelpers");
+            var methodInfo = type.GetMethod("ResizeList", BindingFlags.Static | BindingFlags.Public)
+                .MakeGenericMethod(typeof(T));
+            obj = ResizeListDelegates[typeof(T)] =
+                Delegate.CreateDelegate(typeof(Action<List<T>, int>), methodInfo);
+        }
+ 
+        var action = (Action<List<T>, int>)obj;
+        action.Invoke(list, size);
+    }
+ 
+    private static unsafe void NativeAddRange<T>(this List<T> list, void* arrayBuffer, int length)
+        where T : struct
+    {
+        var index = list.Count;
+        var newLength = index + length;
+ 
+        // Resize our list if we require
+        if (list.Capacity < newLength)
+        {
+            list.Capacity = newLength;
+        }
+ 
+        var items = NoAllocHelpers.ExtractArrayFromListT(list);
+        var size = UnsafeUtility.SizeOf<T>();
+ 
+        // Get the pointer to the end of the list
+        var bufferStart = (IntPtr) UnsafeUtility.AddressOf(ref items[0]);
+        var buffer = (byte*)(bufferStart + (size * index));
+ 
+        UnsafeUtility.MemCpy(buffer, arrayBuffer, length * (long) size);
+ 
+        NoAllocHelpers.ResizeList(list, newLength);
+    }
+
+	private static List<Vector3> verticesList = new List<Vector3>();
+    private static List<Vector3> normalsList = new List<Vector3>();
+    private static List<Vector3> uvsList = new List<Vector3>();
+    private static List<int> trianglesList = new List<int>();
+
+    public unsafe static void SetMesh(
+        Mesh mesh,
+        NativeArray<float3> vertices,
+        NativeArray<float3>? uvs = null,
+        NativeArray<float3>? normals = null,
+        NativeArray<int>? triangles = null)
+    {
+        if (vertices.Length == 0)
+        {
+            return;
+        }
+
+        verticesList.NativeAddRange(vertices.GetUnsafePtr(), vertices.Length);
+		if (uvs.HasValue)
+			uvsList.NativeAddRange(uvs.Value.GetUnsafePtr(), uvs.Value.Length);
+		if (normals.HasValue)
+			normalsList.NativeAddRange(normals.Value.GetUnsafePtr(), normals.Value.Length);
+		if (triangles.HasValue)
+			trianglesList.NativeAddRange(triangles.Value.GetUnsafePtr(), triangles.Value.Length);
+ 
+        mesh.SetVertices(verticesList);
+		if (normals.HasValue)
+			mesh.SetNormals(normalsList);
+		if (uvs.HasValue)
+			mesh.SetUVs(0, uvsList);
+		if (triangles.HasValue)
+			mesh.SetTriangles(trianglesList, 0);
+ 
+        verticesList.Clear();
+        normalsList.Clear();
+        uvsList.Clear();
+        trianglesList.Clear();
+    }
+}
