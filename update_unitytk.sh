a9b9bc81afba5795e2e54260069e504bccaa229c
Kenneth Ellersdorfer <kennux@ymail.com>
2020-04-11 14:35:56 +0200

    Updated VHair to use UnityTK as package.

--- update_unitytk.sh
+++
@@ -1,9 +0,0 @@
-# Helper script that can be used to update UnityTK when using it as git submodule
-# The unity tk code (excluding examples) will be copied to Assets/UnityTK after pulling newest updates from git
-# The git submodule should be stored in UnityTK.
-
-rm -Rf Assets/UnityTK
-cd UnityTK
-git pull origin master
-cd ..
-cp -Rf UnityTK/Assets/UnityTK Assets/UnityTK
